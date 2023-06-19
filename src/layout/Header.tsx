8fe18996aa0c40b86141c5c16ada4ab0c7d50fea
hakyoung <jhy2297@naver.com>
2023-06-19 16:42:10 +0900

    header layout

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -1,9 +1,38 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { BrowserView, MobileView } from 'react-device-detect';
 
 const Header = () => {
   return (
     <header>
-      <h2>헤더</h2>
+      <BrowserView>
+        <div className="flex justify-between h-20" style={{ border: '1px solid red' }}>
+          <ul className="flex justify-center items-center">
+            <li className="mx-3 py-4 cursor-pointer">
+              <Link to="/reservation">RESERVATION</Link>
+            </li>
+            <li className="mx-3 py-4 cursor-pointer">
+              <Link to="/reservation">ABOUT HOTEL</Link>
+            </li>
+            <li className="mx-3 py-4 cursor-pointer">
+              <Link to="/reservation">ACCOMMODATION</Link>
+            </li>
+            <li className="mx-3 py-4 cursor-pointer">
+              <Link to="/reservation">FACILITIES</Link>
+            </li>
+            <li className="mx-3 py-4 cursor-pointer">
+              <Link to="/reservation">SPECIAL OFFER</Link>
+            </li>
+          </ul>
+
+          <ul className="flex justify-end items-center text-sm">
+            <li className="mx-3 py-4 cursor-pointer">로그인</li>
+            <li className="mx-3 py-4 cursor-pointer">예약확인</li>
+            <li className="mx-3 py-4 cursor-pointer">오시는길</li>
+          </ul>
+        </div>
+      </BrowserView>
+      <MobileView>123123123123</MobileView>
     </header>
   );
 };
