c238e903e638afa30b7b813200e4d7dd5822849b
hakyoung <jhy2297@naver.com>
2023-06-19 13:00:16 +0900

    tailwind css + reset

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -3,16 +3,9 @@ import React, { useCallback, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const Main = () => {
-  const [count, setCount] = useState(0);
-
-  const handleOnClick = useCallback(() => {
-    setCount((prevCount) => prevCount + 1);
-  }, []);
-
   return (
     <div>
-      <p>count: {count}</p>
-      <Count onClick={handleOnClick} />
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
       <Link to="/about">
         <div>VITE 번들러 테스트</div>
       </Link>
